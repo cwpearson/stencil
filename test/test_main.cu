4a89b4cdeb3f8ae364f38f75fb354a502ca00aac
Carl Pearson <carl.w.pearson@gmail.com>
2019-12-10 08:11:48 -0600

    .

--- test/test_main.cu
+++ test/test_main.cu
@@ -1,6 +1,7 @@
 //#define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
 //#include "catch2/catch.hpp"
 
+// https://github.com/catchorg/Catch2/blob/master/docs/own-main.md#top
 #define CATCH_CONFIG_RUNNER
 #include "catch2/catch.hpp"
 
