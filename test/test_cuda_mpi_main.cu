61ecd23d79c7cdd4056de24d054ec88d233d5d2d
Carl Pearson <pearson@illinois.edu>
2019-12-19 14:15:05 -0600

    add test_cuda_mpi which is run with mpirun -n 2

---
+++ test/test_cuda_mpi_main.cu
@@ -0,0 +1,20 @@
+//#define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do
+//this in one cpp file #include "catch2/catch.hpp"
+
+// https://github.com/catchorg/Catch2/blob/master/docs/own-main.md#top
+#define CATCH_CONFIG_RUNNER
+#include "catch2/catch.hpp"
+
+#include <mpi.h>
+
+int main(int argc, char *argv[]) {
+  // global setup...
+
+  MPI_Init(&argc, &argv);
+
+  int result = Catch::Session().run(argc, argv);
+
+  MPI_Finalize();
+
+  return result;
+}
