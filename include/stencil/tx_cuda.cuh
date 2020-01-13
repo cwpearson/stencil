2ea35cabb13a1075aa9d6459bb1da2888bb7c6d1
Carl Pearson <pearson@illinois.edu>
2020-01-13 13:43:46 -0600

    add a generic sender/recver pair for contiguous 3D regions

--- include/stencil/tx_cuda.cuh
+++ include/stencil/tx_cuda.cuh
@@ -415,7 +415,6 @@ public:
     }
 
     // unpack all data into halo region
-
     const Dim3 edgePos =
         domain_->edge_pos(dim0_, dim1_, dim0Pos_, dim1Pos_, true /*halo*/);
     const Dim3 edgeExtent = domain_->edge_extent(dim0_, dim1_);
@@ -424,7 +423,6 @@ public:
     for (size_t dataIdx = 0; dataIdx < domain_->num_data(); ++dataIdx) {
 
       char *dst = domain_->curr_data(dataIdx);
-
       // pack into buffer
       dim3 dimGrid(20, 20, 20);
       dim3 dimBlock(32, 4, 4);
@@ -433,6 +431,7 @@ public:
       unpack<<<dimGrid, dimBlock, 0, domain_->stream()>>>(
           dst, rawSz, 0 /*pitch*/, edgePos, edgeExtent, bufs_[dataIdx],
           elemSize);
+      CUDA_RUNTIME(cudaStreamSynchronize(domain_->stream()));
     }
   }
 
@@ -442,7 +441,190 @@ public:
   virtual void wait() override {
     if (fut_.valid()) {
       fut_.wait();
+    } else {
+      assert(0 && "wait called before recv");
+    }
+  }
+};
+
+/*! Send a LocalDomain region using Sender
+ */
+template <typename Sender> class RegionSender : public HaloSender {
+private:
+  const LocalDomain *domain_; // the domain we are sending from
+  Dim3 dir_;                  // the direction vector of the send
+
+  std::future<void> fut_; // future for asyc call to send_impl
+
+  // one sender per domain data
+  std::vector<Sender> senders_;
+  // one flattened device buffer per domain data
+  std::vector<char *> bufs_;
+
+public:
+  RegionSender(const LocalDomain &domain, size_t srcRank, size_t srcGPU,
+               size_t dstRank, size_t dstGPU, Dim3 dir)
+      : domain_(&domain), dir_(dir) {
+
+    assert(dir_.x >= -1 && dir.x <= 1);
+    assert(dir_.y >= -1 && dir.y <= 1);
+    assert(dir_.z >= -1 && dir.z <= 1);
+
+    for (size_t di = 0; di < domain.num_data(); ++di) {
+      senders_.push_back(Sender(srcRank, srcGPU, dstRank, dstGPU, di, dir_));
+    }
+  }
+
+  virtual void allocate() override {
+    // allocate space for each transfer
+    const int gpu = domain_->gpu();
+    CUDA_RUNTIME(cudaSetDevice(gpu));
+
+    // preallocate each sender
+    for (size_t i = 0; i < domain_->num_data(); ++i) {
+      size_t numBytes = domain_->halo_bytes(dir_, i);
+      char *buf = nullptr;
+      CUDA_RUNTIME(cudaMalloc(&buf, numBytes));
+      bufs_.push_back(buf);
+      senders_[i].resize(numBytes);
+    }
+  }
+
+  void send_impl() {
+    assert(bufs_.size() == senders_.size() && "was allocate called?");
+    const Dim3 haloPos = domain_->halo_pos(dir_, false /*compute region*/);
+    const Dim3 haloExtent = domain_->halo_extent(dir_);
+
+    const Dim3 rawSz = domain_->raw_size();
+    for (size_t idx = 0; idx < domain_->num_data(); ++idx) {
+      const char *src = domain_->curr_data(idx);
+      const size_t elemSize = domain_->elem_size(idx);
+
+      // pack into buffer
+      dim3 dimGrid(20, 20, 20);
+      dim3 dimBlock(32, 4, 4);
+
+      CUDA_RUNTIME(cudaSetDevice(domain_->gpu()));
+      pack<<<dimGrid, dimBlock, 0, domain_->stream()>>>(
+          bufs_[idx], src, rawSz, 0 /*pitch*/, haloPos, haloExtent, elemSize);
+    }
+    // wait for packs
+    CUDA_RUNTIME(cudaStreamSynchronize(domain_->stream()));
+
+    for (size_t dataIdx = 0; dataIdx < domain_->num_data(); ++dataIdx) {
+      // copy to dst rank
+      printf("RegionSender::send_impl(): send data %lu\n", dataIdx);
+      senders_[dataIdx].send(bufs_[dataIdx]);
+    }
+
+    // wait for sends
+    std::cerr << "RegionSender::wait() for senders\n";
+    for (auto &s : senders_) {
+      s.wait();
+    }
+  }
+
+  void send() override { fut_ = std::async(&RegionSender::send_impl, this); }
+
+  // wait for send to be complete
+  void wait() override {
+    if (fut_.valid()) {
+      std::cout << "RegionSender::wait() for fut_\n";
+      fut_.wait();
+      std::cout << "RegionSender::wait() done\n";
+    } else {
+      assert(0 && "wait called before send()");
+    }
+  }
+};
+
+/*! \brief Receive a LocalDomain face using Recver
+ */
+template <typename Recver> class RegionRecver : public HaloRecver {
+private:
+  const LocalDomain *domain_; // the domain we are receiving into
+  Dim3 dir_;                  // the direction of the send we are recving
+
+  std::future<void> fut_;
+
+  // one receiver per domain data
+  std::vector<Recver> recvers_;
+  // one device buffer per domain data
+  std::vector<char *> bufs_;
+
+public:
+  RegionRecver(const LocalDomain &domain, size_t srcRank, size_t srcGPU,
+               size_t dstRank, size_t dstGPU, const Dim3 &dir)
+      : domain_(&domain), dir_(dir) {
+
+    assert(dir_.x >= -1 && dir.x <= 1);
+    assert(dir_.y >= -1 && dir.y <= 1);
+    assert(dir_.z >= -1 && dir.z <= 1);
+
+    for (size_t di = 0; di < domain.num_data(); ++di) {
+      recvers_.push_back(Recver(srcRank, srcGPU, dstRank, dstGPU, di, dir_));
+    }
+  }
+
+  virtual void allocate() override {
+    // allocate space for each transfer
+    const int gpu = domain_->gpu();
+    CUDA_RUNTIME(cudaSetDevice(gpu));
+
+    for (size_t i = 0; i < domain_->num_data(); ++i) {
+      size_t numBytes = domain_->halo_bytes(dir_, i);
+      std::cerr << "allocate " << numBytes << "\n";
+      char *buf = nullptr;
+      CUDA_RUNTIME(cudaMalloc(&buf, numBytes));
+      bufs_.push_back(buf);
+      recvers_[i].resize(numBytes);
+    }
+  }
+
+  void recv_impl() {
+    assert(bufs_.size() == recvers_.size());
+
+    // receive all data into flat buffers
+    for (size_t dataIdx = 0; dataIdx < bufs_.size(); ++dataIdx) {
+      recvers_[dataIdx].recv(bufs_[dataIdx]);
+    }
+
+    // wait for all data
+    for (auto &r : recvers_) {
+      r.wait();
+    }
+
+    // unpack all data into domain's halo
+    const Dim3 haloPos = domain_->halo_pos(dir_, true /*halo region*/);
+    const Dim3 haloExtent = domain_->halo_extent(dir_);
+
+    const Dim3 rawSz = domain_->raw_size();
+    for (size_t dataIdx = 0; dataIdx < domain_->num_data(); ++dataIdx) {
+
+      char *dst = domain_->curr_data(dataIdx);
+
+      // unpack from buffer into halo
+      dim3 dimGrid(20, 20, 20);
+      dim3 dimBlock(32, 4, 4);
+      size_t elemSize = domain_->elem_size(dataIdx);
+      CUDA_RUNTIME(cudaSetDevice(domain_->gpu()));
+
+      unpack<<<dimGrid, dimBlock, 0, domain_->stream()>>>(
+          dst, rawSz, 0 /*pitch*/, haloPos, haloExtent, bufs_[dataIdx],
+          elemSize);
+
       CUDA_RUNTIME(cudaStreamSynchronize(domain_->stream()));
+    }
+  }
+
+  void recv() override { fut_ = std::async(&RegionRecver::recv_impl, this); }
+
+  // wait for send to be complete
+  virtual void wait() override {
+    if (fut_.valid()) {
+      std::cerr << "RegionRecver::wait() for fut_\n";
+      fut_.wait();
+      std::cerr << "RegionRecver::wait() done\n";
     } else {
       assert(0 && "wait called before recv");
     }
