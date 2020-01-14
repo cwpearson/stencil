71325b4af2a19e8c4fcf09a403d8fd3f3ebe7655
Carl Pearson <pearson@illinois.edu>
2020-01-14 15:19:36 -0600

    all ranks try to enable peer access for all GPUs

--- include/stencil/stencil.hpp
+++ include/stencil/stencil.hpp
@@ -131,21 +131,25 @@ public:
       colocated_.insert(r);
     }
     assert(colocated_.count(rank_) == 1 && "should be colocated with self");
-    printf("DistributedDomain::ctor(): rank %d colocated with %lu ranks\n",
+    printf("DistributedDomain::ctor(): rank %d colocated with %lu other ranks\n",
            rank_, colocated_.size() - 1);
 
     // Try to enable peer access between all GPUs
     nvtxRangePush("peer_en");
-    peerAccess_ = std::vector<std::vector<bool>>(
-        gpus_.size(), std::vector<bool>(gpus_.size(), false));
 
-    for (auto &src : gpus_) {
-      for (auto &dst : gpus_) {
+    // can't use gpus_.size() because we don't own all the GPUs
+    int count;
+    CUDA_RUNTIME(cudaGetDeviceCount(&count));
+    peerAccess_ = std::vector<std::vector<bool>>(
+        count, std::vector<bool>(count, false));
+
+    for (int src = 0; src < count; ++src) {
+      for (int dst = 0; dst < count; ++dst) {
         CUDA_RUNTIME(cudaSetDevice(src))
         cudaError_t err = cudaDeviceEnablePeerAccess(dst, 0 /*flags*/);
         if (cudaSuccess == err || cudaErrorPeerAccessAlreadyEnabled == err) {
           peerAccess_[src][dst] = true;
-          std::cout << src << " -> " << dst << "peer access\n";
+          std::cout << src << " -> " << dst << " peer access\n";
         } else if (cudaErrorInvalidDevice) {
           peerAccess_[src][dst] = false;
         } else {
