35b6b513b688cbea14668ce93214ee8e26d75c70
Carl Pearson <pearson@illinois.edu>
2020-01-18 06:04:51 -0600

    tests

---
+++ test/test_cuda_mtregiontx.cu
@@ -0,0 +1,73 @@
+#include "catch2/catch.hpp"
+
+#include "stencil/local_domain.cuh"
+#include "stencil/tx_cuda.cuh"
+
+TEMPLATE_TEST_CASE("MTRegionSender MTRegionRecver", "[cuda][mpi][template]", int,
+                   double) {
+
+  const Dim3 sz(30, 40, 50);
+  const int gpu = 0;
+  const size_t radius = 4;
+  LocalDomain d0(sz, gpu);
+  LocalDomain d1(sz, gpu);
+
+  d0.set_radius(radius);
+  d1.set_radius(radius);
+
+  auto h0 = d0.add_data<TestType>();
+  auto h1 = d1.add_data<TestType>();
+
+  d0.realize();
+  d1.realize();
+
+  const int srcRank = 0;
+  const int dstRank = 0;
+  const int srcGPU = 0;
+  const int dstGPU = 0;
+
+  SECTION("+x +y +z corner") {
+    std::cerr << "start +x +y +z corner test\n";
+    Dim3 dir(1, 1, 1);
+
+    MTRegionSender<AnySender> sender(d0, srcRank, srcGPU, dstRank, dstGPU, dir);
+    MTRegionRecver<AnyRecver> recver(d1, srcRank, srcGPU, dstRank, dstGPU, dir);
+
+    sender.allocate();
+    recver.allocate();
+    sender.send();
+    recver.recv();
+    sender.wait();
+    recver.wait();
+  }
+
+  SECTION("+x +y edge") {
+    std::cerr << "start +x +y edge test\n";
+    Dim3 dir(1, 1, 0);
+
+    MTRegionSender<AnySender> sender(d0, srcRank, srcGPU, dstRank, dstGPU, dir);
+    MTRegionRecver<AnyRecver> recver(d1, srcRank, srcGPU, dstRank, dstGPU, dir);
+
+    sender.allocate();
+    recver.allocate();
+    sender.send();
+    recver.recv();
+    sender.wait();
+    recver.wait();
+  }
+
+  SECTION("+x face") {
+    std::cerr << "start +x face test\n";
+    Dim3 dir(1, 0, 0);
+
+    MTRegionSender<AnySender> sender(d0, srcRank, srcGPU, dstRank, dstGPU, dir);
+    MTRegionRecver<AnyRecver> recver(d1, srcRank, srcGPU, dstRank, dstGPU, dir);
+
+    sender.allocate();
+    recver.allocate();
+    sender.send();
+    recver.recv();
+    sender.wait();
+    recver.wait();
+  }
+}
