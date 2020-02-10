10b058a9fc3dfa86d034198d83605d8276f366c2
Carl Pearson <pearson@illinois.edu>
2020-02-10 10:18:39 -0600

    mat2d convenience

--- include/stencil/mat2d.hpp
+++ include/stencil/mat2d.hpp
@@ -3,3 +3,7 @@
 #include <vector>
 
 template <typename T> using Mat2D = std::vector<std::vector<T>>;
+
+template <typename T> Mat2D<T> make_mat2d(size_t n, const T &val) {
+  return std::vector<std::vector<T>>(n, std::vector<T>(n, val));
+}
