bd894cb8b544bc62d85cc462d95e89dd5187c0aa
Mert Hidayetoglu <merth@batch3.summit.olcf.ornl.gov>
2020-02-10 15:30:01 -0500

    add comment

--- scripts/summit/run.sh
+++ scripts/summit/run.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
+#create file here: /gpfs/alpine/scratch/merth/csc362/
+#then you can mv /gpfs/alpine/scratch/merth/csc362/ .
+
 export OMP_NUM_THREADS=1
-jsrun -n 1 -r 1 -c 1 -g 6 -a 6 -b rs js_task_info ../../build/src/weak
+jsrun -n 6 -r 6 -c 1 -g 1 -a 1 -b rs js_task_info ../../build/src/weak
