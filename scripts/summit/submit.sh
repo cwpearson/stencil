415a3575e421c3cfe0b8e48bf73329a06ce050cb
Mert Hidayetoglu <merth@login3.summit.olcf.ornl.gov>
2020-02-11 11:19:15 -0500

    set GPU execution mode to default

--- scripts/summit/submit.sh
+++ scripts/summit/submit.sh
@@ -1,1 +1,1 @@
-bsub -W 01:00 -nnodes 1 -P CSC362 -env "all,LSF_CPU_ISOLATION=on" -Is /bin/bash
+bsub -W 01:00 -nnodes 1 -P CSC362 -alloc_flags gpudefault -env "all,LSF_CPU_ISOLATION=on" -Is /bin/bash
