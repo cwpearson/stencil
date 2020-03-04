d43268b5c31cc23b39c8f3a856391e301d6c3be3
Carl Pearson <pearson@illinois.edu>
2020-03-04 15:40:02 -0600

    reduce repeated allcoations

--- src/measure_buf_exchange.cu
+++ src/measure_buf_exchange.cu
@@ -7,21 +7,21 @@
 #include "stencil/cuda_runtime.hpp"
 #include "stencil/mat2d.hpp"
 
-__global__ void clock_block(clock_t *d, clock_t clock_count)
-{
-    clock_t start_clock = clock64();
-    clock_t clock_offset = 0;
-    while (clock_offset < clock_count)
-    {
-        clock_offset = clock64() - start_clock;
-    }
-    if (d) {
-     *d = clock_offset;
-    }
+__global__ void clock_block(clock_t *d, clock_t clock_count) {
+  clock_t start_clock = clock64();
+  clock_t clock_offset = 0;
+  while (clock_offset < clock_count) {
+    clock_offset = clock64() - start_clock;
+  }
+  if (d) {
+    *d = clock_offset;
+  }
 }
 
-
 int main(int argc, char **argv) {
+
+  (void)argc;
+  (void)argv;
 
   const int64_t K = 1024;
   const int64_t M = K * K;
@@ -32,7 +32,7 @@ int main(int argc, char **argv) {
   const int nIters = 50;
 
   const double period = 0.004;
- 
+
   // enable peer access
   for (size_t src = 0; src < nGpus; ++src) {
     for (size_t dst = 0; dst < nGpus; ++dst) {
@@ -77,7 +77,7 @@ int main(int argc, char **argv) {
   }
 
   // bytes to send
-  Mat2D<int64_t> x(nGpus, nGpus, 8*M);
+  Mat2D<int64_t> x(nGpus, nGpus, 8 * M);
 
   // update to x
   Mat2D<int64_t> dx(nGpus, nGpus, 0);
@@ -87,50 +87,55 @@ int main(int argc, char **argv) {
 
   int64_t cycles = 1e5;
   for (int iter = 0; iter < nIters; ++iter) {
-  
+
     std::cout << "x\n";
     for (size_t i = 0; i < nGpus; ++i) {
       for (size_t j = 0; j < nGpus; ++j) {
-        printf("%ld ", x.at(i,j) / 1024 / 1024);
+        printf("%ld ", x.at(i, j) / 1024 / 1024);
       }
       std::cout << "\n";
     }
 
-    // set up buffers, 
+    // set up buffers,
     for (int i = 0; i < nGpus; ++i) {
       for (int j = 0; j < nGpus; ++j) {
-	if (bufSizes.at(i,j) < x.at(i,j)) {
-	  bufSizes.at(i,j) = x.at(i,j) * 2;
-	  std::cout << "increasing buffer size to " << bufSizes.at(i,j) << "\n";
-	  CUDA_RUNTIME(cudaFree(srcBufs.at(i,j)));
-	  CUDA_RUNTIME(cudaFree(dstBufs.at(i,j)));
+        if (bufSizes.at(i, j) < x.at(i, j)) {
+          bufSizes.at(i, j) = x.at(i, j) * 2;
+          std::cout << "increasing buffer size to " << bufSizes.at(i, j)
+                    << "\n";
+          CUDA_RUNTIME(cudaFree(srcBufs.at(i, j)));
+          CUDA_RUNTIME(cudaFree(dstBufs.at(i, j)));
           CUDA_RUNTIME(cudaSetDevice(i));
-          CUDA_RUNTIME(cudaMalloc(&srcBufs.at(i, j), bufSizes.at(i,j)));	  
+          CUDA_RUNTIME(cudaMalloc(&srcBufs.at(i, j), bufSizes.at(i, j)));
           CUDA_RUNTIME(cudaSetDevice(j));
           CUDA_RUNTIME(cudaMalloc(&dstBufs.at(i, j), bufSizes.at(i, j)));
-	}
+        }
       }
     }
-
 
     y = Mat2D<double>(nGpus, nGpus, 0);
     for (int n = 0; n < nMeasures; ++n) {
 
       // set up copies and wait on latch event.
-      // grow the wait time before the latch event until it covers the copy setup time
+      // grow the wait time before the latch event until it covers the copy
+      // setup time
       while (true) {
         CUDA_RUNTIME(cudaSetDevice(0));
         CUDA_RUNTIME(cudaEventDestroy(latch));
         CUDA_RUNTIME(cudaEventCreate(&latch));
-        clock_block<<<1,1,0,0>>>(nullptr, cycles);
+        clock_block<<<1, 1, 0, 0>>>(nullptr, cycles);
         CUDA_RUNTIME(cudaEventRecord(latch, 0));
         for (int i = 0; i < nGpus; ++i) {
           for (int j = 0; j < nGpus; ++j) {
             CUDA_RUNTIME(cudaSetDevice(i));
-            CUDA_RUNTIME(cudaStreamWaitEvent(streams.at(i,j), latch, 0));
-            CUDA_RUNTIME(cudaEventRecord(startEvents.at(i,j), streams.at(i,j)));
-            CUDA_RUNTIME(cudaMemcpyPeerAsync(dstBufs.at(i,j), j, srcBufs.at(i,j), i, x.at(i,j), streams.at(i,j)));
-            CUDA_RUNTIME(cudaEventRecord(stopEvents.at(i,j), streams.at(i,j)));
+            CUDA_RUNTIME(cudaStreamWaitEvent(streams.at(i, j), latch, 0));
+            CUDA_RUNTIME(
+                cudaEventRecord(startEvents.at(i, j), streams.at(i, j)));
+            CUDA_RUNTIME(cudaMemcpyPeerAsync(dstBufs.at(i, j), j,
+                                             srcBufs.at(i, j), i, x.at(i, j),
+                                             streams.at(i, j)));
+            CUDA_RUNTIME(
+                cudaEventRecord(stopEvents.at(i, j), streams.at(i, j)));
           }
         }
 
@@ -153,20 +158,19 @@ int main(int argc, char **argv) {
         }
       }
 
-       // wait for copies to finish
-       for (size_t i = 0; i < nGpus; ++i) {
-         for (size_t j = 0; j < nGpus; ++j) {
-           CUDA_RUNTIME(cudaStreamSynchronize(streams.at(i, j)));
-         }
-       }
-
+      // wait for copies to finish
+      for (size_t i = 0; i < nGpus; ++i) {
+        for (size_t j = 0; j < nGpus; ++j) {
+          CUDA_RUNTIME(cudaStreamSynchronize(streams.at(i, j)));
+        }
+      }
 
       // get times
       for (size_t i = 0; i < nGpus; ++i) {
         for (size_t j = 0; j < nGpus; ++j) {
           float ms;
           CUDA_RUNTIME(cudaEventElapsedTime(&ms, startEvents.at(i, j),
-                                              stopEvents.at(i, j)));
+                                            stopEvents.at(i, j)));
           y.at(i, j) += ms / 1000.0;
         }
       }
@@ -176,17 +180,16 @@ int main(int argc, char **argv) {
     std::cout << "y\n";
     for (size_t i = 0; i < nGpus; ++i) {
       for (size_t j = 0; j < nGpus; ++j) {
-        printf("%.4e ", y.at(i,j));
+        printf("%.4e ", y.at(i, j));
       }
       std::cout << "\n";
     }
-
 
     // check if times are all close to target
     bool done = true;
     for (size_t i = 0; i < nGpus; ++i) {
       for (size_t j = 0; j < nGpus; ++j) {
-        if (std::abs(y.at(i,j) - period) > period / 100.0) {
+        if (std::abs(y.at(i, j) - period) > period / 100.0) {
           done = false;
         }
       }
@@ -198,15 +201,15 @@ int main(int argc, char **argv) {
     // take a step in moving each time towards the target time
     for (size_t i = 0; i < nGpus; ++i) {
       for (size_t j = 0; j < nGpus; ++j) {
-        double dydx = y.at(i,j) / x.at(i,j);
-        dx.at(i,j) = (period - y.at(i,j)) / dydx;
+        double dydx = y.at(i, j) / x.at(i, j);
+        dx.at(i, j) = (period - y.at(i, j)) / dydx;
       }
     }
-    
+
     std::cout << "dx\n";
     for (size_t i = 0; i < nGpus; ++i) {
       for (size_t j = 0; j < nGpus; ++j) {
-        printf("%ld ", dx.at(i,j));
+        printf("%ld ", dx.at(i, j));
       }
       std::cout << "\n";
     }
@@ -215,19 +218,16 @@ int main(int argc, char **argv) {
     double gamma = 0.2;
     for (size_t i = 0; i < nGpus; ++i) {
       for (size_t j = 0; j < nGpus; ++j) {
-        x.at(i,j) += gamma * dx.at(i,j);
+        x.at(i, j) += gamma * dx.at(i, j);
       }
     }
-
   }
 
   // print final communication sizes
   for (size_t i = 0; i < nGpus; ++i) {
     for (size_t j = 0; j < nGpus; ++j) {
-	    std::cout << x.at(i,j) /1024 / 1024 << " ";
+      std::cout << x.at(i, j) / 1024 / 1024 << " ";
     }
     std::cout << "\n";
   }
-
-
-} 
+}
