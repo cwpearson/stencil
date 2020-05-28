d065f20d8768d7d5d767fb801b1d28bd6f1dbfee
Carl Pearson <carl.w.pearson@gmail.com>
2020-05-28 15:47:33 -0500

    add Accessor<T>

---
+++ include/stencil/accessor.hpp
@@ -0,0 +1,44 @@
+#pragma once
+
+#include <cassert>
+
+#include "stencil/dim3.hpp"
+
+#ifdef __CUDACC__
+#define CUDA_CALLABLE_MEMBER __host__ __device__
+#else
+#define CUDA_CALLABLE_MEMBER
+#endif
+
+template <typename T> class Accessor {
+private:
+  T *raw_;
+  Dim3 origin_; // the 3D point in the space represented by offset 0
+  Dim3 pitch_;  // TODO: make this pitch bytes instead of elements
+
+public:
+  Accessor(T *raw, const Dim3 &origin,
+           const Dim3 &pitch //<! [in] pitch in elements of allocation
+           )
+      : raw_(raw), origin_(origin), pitch_(pitch) {}
+
+  CUDA_CALLABLE_MEMBER T &operator[](const Dim3 &p) noexcept {
+    const Dim3 off = p - origin_;
+#ifndef NDEBUG
+    assert(off.x >= 0);
+    assert(off.y >= 0);
+    assert(off.z >= 0);
+#endif
+    return raw_[off.z * pitch_.y * pitch_.x + off.y * pitch_.x + off.x];
+  }
+
+  CUDA_CALLABLE_MEMBER const T &operator[](const Dim3 &p) const noexcept {
+    const Dim3 off = p - origin_;
+    return raw_[off.z * pitch_.y * pitch_.x + off.y * pitch_.x + off.x];
+  }
+
+  const Dim3 &origin() const noexcept {return origin_;}
+  const Dim3 &pitch() const noexcept {return pitch_;}
+};
+
+#undef CUDA_CALLABLE_MEMBER
---
+++ test/test_cuda_accessor.cu
@@ -0,0 +1,30 @@
+#include "catch2/catch.hpp"
+
+#include <cstring> // std::memcpy
+
+#include "stencil/accessor.hpp"
+
+TEMPLATE_TEST_CASE("accessor", "[template]", int, double) {
+  Dim3 extent(10, 11, 12);
+  TestType *data = new TestType[extent.x * extent.y * extent.z];
+  {
+    Dim3 origin(0, 0, 0);
+    Accessor<TestType> acc(data, origin, extent);
+    acc[Dim3(0, 0, 0)] = 10;
+    REQUIRE(acc[Dim3(0, 0, 0)] == 10);
+    REQUIRE(data[0] == 10);
+
+    acc[Dim3(9, 10, 11)] = 13;
+    REQUIRE(acc[Dim3(9, 10, 11)] == 13);
+    REQUIRE(data[11 * (10 * 11) + 10 * (10) + 9] == 13);
+  }
+
+  // correct origin offset
+  {
+    Dim3 origin(1, 1, 1);
+    Accessor<TestType> acc(data, origin, extent);
+    REQUIRE(acc[Dim3(1, 1, 1)] == 10);
+  }
+
+  delete[] data;
+}
