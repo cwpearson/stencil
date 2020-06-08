0e269bf142d589ab321218310167a2401d5e3443
Carl Pearson <carl.w.pearson@gmail.com>
2020-06-08 14:20:29 -0500

    add missing include

--- include/stencil/radius.hpp
+++ include/stencil/radius.hpp
@@ -6,6 +6,7 @@
 #include <iostream>
 
 #include "stencil/direction_map.hpp"
+#include "stencil/dim3.hpp"
 
 // #define SPEW(x) std::cerr << "SPEW[" << __FILE__ << ":" << __LINE__ << "] " <<  x << "\n";
 #define SPEW(x)
@@ -15,8 +16,11 @@ private:
   DirectionMap<size_t> rads_;
 
 public:
-  size_t &dir(int x, int y, int z) { return rads_.at_dir(x, y, z); }
-  const size_t &dir(int x, int y, int z) const { return rads_.at_dir(x, y, z); }
+  size_t &dir(int x, int y, int z) noexcept { return rads_.at_dir(x, y, z); }
+  const size_t &dir(int x, int y, int z) const noexcept  { return rads_.at_dir(x, y, z); }
+  size_t &dir(const Dim3 &d) noexcept { return dir(d.x, d.y, d.z); }
+  const size_t &dir(const Dim3 &d) const noexcept { return dir(d.x, d.y, d.z); }
+
 
   const size_t &x(int d) const noexcept {
     assert(d >= -1);
