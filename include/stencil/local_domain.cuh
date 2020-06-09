86d2c947fe2ca53ed9629f80f64ea5388f8005d2
Carl Pearson <carl.w.pearson@gmail.com>
2020-06-09 14:47:15 -0500

    improve logging

--- include/stencil/local_domain.cuh
+++ include/stencil/local_domain.cuh
@@ -10,8 +10,52 @@
 #include "stencil/radius.hpp"
 #include "stencil/rect3.hpp"
 
-#define LOG_SPEW(x) std::cerr << "SPEW[" << __FILE__ << ":" << __LINE__ << ": " <<  x << "\n";
-// #define LOG_SPEW(x)
+#ifndef STENCIL_OUTPUT_LEVEL
+#define STENCIL_OUTPUT_LEVEL 0
+#endif
+
+#if STENCIL_OUTPUT_LEVEL <= 0
+#define LOG_SPEW(x)                                                            \
+  std::cerr << "SPEW[" << __FILE__ << ":" << __LINE__ << "] " << x << "\n";
+#else
+#define LOG_SPEW(x)
+#endif
+
+#if STENCIL_OUTPUT_LEVEL <= 1
+#define LOG_DEBUG(x)                                                           \
+  std::cerr << "DEBUG[" << __FILE__ << ":" << __LINE__ << "] " << x << "\n";
+#else
+#define LOG_DEBUG(x)
+#endif
+
+#if STENCIL_OUTPUT_LEVEL <= 2
+#define LOG_INFO(x)                                                            \
+  std::cerr << "INFO[" << __FILE__ << ":" << __LINE__ << "] " << x << "\n";
+#else
+#define LOG_INFO(x)
+#endif
+
+#if STENCIL_OUTPUT_LEVEL <= 3
+#define LOG_WARN(x)                                                            \
+  std::cerr << "WARN[" << __FILE__ << ":" << __LINE__ << "] " << x << "\n";
+#else
+#define LOG_WARN(x)
+#endif
+
+#if STENCIL_OUTPUT_LEVEL <= 4
+#define LOG_ERROR(x)                                                           \
+  std::cerr << "ERROR[" << __FILE__ << ":" << __LINE__ << "] " << x << "\n";
+#else
+#define LOG_ERROR(x)
+#endif
+
+#if STENCIL_OUTPUT_LEVEL <= 5
+#define LOG_FATAL(x)                                                           \
+  std::cerr << "FATAL[" << __FILE__ << ":" << __LINE__ << "] " << x << "\n";   \
+  exit(1);
+#else
+#define LOG_FATAL(x) exit(1);
+#endif
 
 class DistributedDomain;
 
@@ -444,3 +488,8 @@ public:
 };
 
 #undef LOG_SPEW
+#undef LOG_DEBUG
+#undef LOG_INFO
+#undef LOG_WARN
+#undef LOG_ERROR
+#undef LOG_FATAL
