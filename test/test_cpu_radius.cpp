76ab6d3b1e34161d6cbaa4d15dbdb2e7f5674669
Carl Pearson <carl.w.pearson@gmail.com>
2020-07-30 15:55:25 -0600

    improve Radius tests

--- test/test_cpu_radius.cpp
+++ test/test_cpu_radius.cpp
@@ -6,8 +6,6 @@ TEST_CASE("radius") {
 
   Radius r0, r1;
 
-
-
   SECTION("copy") {
     Radius r3 = Radius::constant(1);
     Radius r2(r3);
@@ -15,11 +13,23 @@ TEST_CASE("radius") {
   }
 
   SECTION("constant") {
-  r0 = Radius::constant(0);
-  r1 = Radius::constant(0);
+    r0 = Radius::constant(0);
+    r1 = Radius::constant(0);
 
-  REQUIRE(r0.dir(0, 0, 0) == r1.dir(0, 0, 0));
-  REQUIRE(r0.x(-1) == 0);
-  REQUIRE(r0.x(1) == 0);
+    REQUIRE(r0.dir(0, 0, 0) == r1.dir(0, 0, 0));
+    REQUIRE(r0.x(-1) == 0);
+    REQUIRE(r0.x(1) == 0);
+  }
+
+  SECTION("face") {
+    r0 = Radius::constant(0);
+    r0.set_face(1);
+
+    REQUIRE(r0.x(-1) == 1);
+    REQUIRE(r0.x(1) == 1);
+    REQUIRE(r0.y(-1) == 1);
+    REQUIRE(r0.y(1) == 1);
+    REQUIRE(r0.z(-1) == 1);
+    REQUIRE(r0.z(1) == 1);
   }
 }
