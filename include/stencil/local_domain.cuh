35b086371283cbaf173b3b9fafc3ca310183ade2
Carl Pearson <carl.w.pearson@gmail.com>
2020-08-06 13:31:20 -0600

    move code out of header

--- include/stencil/local_domain.cuh
+++ include/stencil/local_domain.cuh
@@ -272,46 +272,7 @@ public:
   // return the position of the halo relative to get_data() on the `dir` side of the
   // LocalDomain (e.g., dir [1,0,0] returns the position of the region on the +x side)
   // dir = [0,0,0] returns the entire region (without the halo)
-  Dim3 halo_pos(const Dim3 &dir, const bool halo) const noexcept {
-    assert(dir.all_gt(-2));
-    assert(dir.all_lt(2));
-
-    Dim3 ret;
-
-    // +xhalo is the left edge + -x radius + the interior
-    // +x interior is just the left edge + interior size
-    if (1 == dir.x) {
-      ret.x = sz_.x + (halo ? radius_.x(-1) : 0);
-    } else if (-1 == dir.x) {
-      ret.x = halo ? 0 : radius_.x(-1);
-    } else if (0 == dir.x) {
-      ret.x = radius_.x(-1);
-    } else {
-      __builtin_unreachable();
-    }
-
-    if (1 == dir.y) {
-      ret.y = sz_.y + (halo ? radius_.y(-1) : 0);
-    } else if (-1 == dir.y) {
-      ret.y = halo ? 0 : radius_.y(-1);
-    } else if (0 == dir.y) {
-      ret.y = radius_.y(-1);
-    } else {
-      __builtin_unreachable();
-    }
-
-    if (1 == dir.z) {
-      ret.z = sz_.z + (halo ? radius_.z(-1) : 0);
-    } else if (-1 == dir.z) {
-      ret.z = halo ? 0 : radius_.z(-1);
-    } else if (0 == dir.z) {
-      ret.z = radius_.z(-1);
-    } else {
-      __builtin_unreachable();
-    }
-
-    return ret;
-  }
+  Dim3 halo_pos(const Dim3 &dir, const bool halo) const noexcept;
 
   /* return the coordinates of the halo region on side `dir`.
      `halo` determines whether exterior (false) or halo (true) coordinates
@@ -357,32 +318,11 @@ public:
    */
   void swap() noexcept;
 
+  /* return the bytes making up
+  */
   std::vector<unsigned char> region_to_host(const Dim3 &pos, const Dim3 &ext,
                                             const size_t qi // quantity index
-                                            ) const {
-
-    const size_t bytes = elem_size(qi) * ext.flatten();
-
-    // pack quantity
-    CUDA_RUNTIME(cudaSetDevice(gpu()));
-    void *devBuf = nullptr;
-    CUDA_RUNTIME(cudaMalloc(&devBuf, bytes));
-    const dim3 dimBlock = Dim3::make_block_dim(ext, 512);
-    const dim3 dimGrid = (ext + Dim3(dimBlock) - 1) / (Dim3(dimBlock));
-    pack_kernel<<<dimGrid, dimBlock>>>(devBuf, curr_data(qi), raw_size(), pos, ext, elem_size(qi));
-    CUDA_RUNTIME(cudaDeviceSynchronize());
-
-    // copy quantity to host
-    std::vector<unsigned char> hostBuf(bytes);
-    CUDA_RUNTIME(cudaMemcpy(hostBuf.data(), devBuf, hostBuf.size(), cudaMemcpyDefault));
-
-    float *ptr = reinterpret_cast<float *>(hostBuf.data());
-
-    // free device buffer
-    CUDA_RUNTIME(cudaFree(devBuf));
-
-    return hostBuf;
-  }
+                                            ) const;
 
   /*! Copy the compute region to the host
    */
@@ -405,50 +345,5 @@ public:
     return region_to_host(Dim3(0, 0, 0), allocSz, qi);
   }
 
-  void realize() {
-    LOG_SPEW("in realize()");
-    CUDA_RUNTIME(cudaGetLastError());
-    assert(currDataPtrs_.size() == nextDataPtrs_.size());
-    assert(dataElemSize_.size() == nextDataPtrs_.size());
-
-    LOG_INFO("origin is " << origin_);
-
-    // allocate each data region
-    CUDA_RUNTIME(cudaSetDevice(dev_));
-    // int rank;
-    // MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-    // std::cerr << "r" << rank << " dev=" << dev_ << "\n";
-    for (int64_t i = 0; i < num_data(); ++i) {
-      assert(i < dataElemSize_.size());
-      int64_t elemSz = dataElemSize_[i];
-      LOG_SPEW("elemSz=" << elemSz);
-      LOG_SPEW("radius +x=" << radius_.x(1));
-      LOG_SPEW("radius -x=" << radius_.x(-1));
-      LOG_SPEW("radius +y=" << radius_.y(1));
-      LOG_SPEW("radius -y=" << radius_.y(-1));
-      LOG_SPEW("radius +z=" << radius_.z(1));
-      LOG_SPEW("radius -z=" << radius_.z(-1));
-
-      int64_t elemBytes = ((sz_.x + radius_.x(-1) + radius_.x(1)) * (sz_.y + radius_.y(-1) + radius_.y(1)) *
-                           (sz_.z + radius_.z(-1) + radius_.z(1))) *
-                          elemSz;
-      LOG_SPEW("allocate " << elemBytes << " bytes");
-      char *c = nullptr;
-      char *n = nullptr;
-      CUDA_RUNTIME(cudaMalloc(&c, elemBytes));
-      CUDA_RUNTIME(cudaMalloc(&n, elemBytes));
-      assert(uintptr_t(c) % elemSz == 0 && "allocation should be aligned");
-      assert(uintptr_t(n) % elemSz == 0 && "allocation should be aligned");
-      currDataPtrs_[i] = c;
-      nextDataPtrs_[i] = n;
-    }
-
-    CUDA_RUNTIME(cudaMalloc(&devCurrDataPtrs_, currDataPtrs_.size() * sizeof(currDataPtrs_[0])));
-    CUDA_RUNTIME(cudaMalloc(&devDataElemSize_, dataElemSize_.size() * sizeof(dataElemSize_[0])));
-    CUDA_RUNTIME(cudaMemcpy(devCurrDataPtrs_, currDataPtrs_.data(), currDataPtrs_.size() * sizeof(currDataPtrs_[0]),
-                            cudaMemcpyHostToDevice));
-    CUDA_RUNTIME(cudaMemcpy(devDataElemSize_, dataElemSize_.data(), dataElemSize_.size() * sizeof(dataElemSize_[0]),
-                            cudaMemcpyHostToDevice));
-    CUDA_RUNTIME(cudaGetLastError());
-  }
+  void realize();
 };
--- src/local_domain.cu
+++ src/local_domain.cu
@@ -7,13 +7,13 @@ void LocalDomain::set_device(CudaErrorsFatal fatal) {
   if (CudaErrorsFatal::YES == fatal) {
     CUDA_RUNTIME(err)
   } else {
-    (void) err;
+    (void)err;
   }
 }
 
 Rect3 LocalDomain::halo_coords(const Dim3 &dir, const bool halo) const {
   /* convert halo position as offset from allocation to coordinates
-  */
+   */
 
   // get the offset of the halo from the allocation
   Dim3 pos = halo_pos(dir, halo);
@@ -21,7 +21,7 @@ Rect3 LocalDomain::halo_coords(const Dim3 &dir, const bool halo) const {
 
   // convert to offset from the origin
   // the size of the negative halo is equal to the size of the negative kernel radius
-  // translate 
+  // translate
   pos.z -= radius_.z(-1);
   pos.y -= radius_.y(-1);
   pos.x -= radius_.x(-1);
@@ -29,7 +29,7 @@ Rect3 LocalDomain::halo_coords(const Dim3 &dir, const bool halo) const {
   // shift by the origin
   pos += origin_;
 
-  return Rect3(pos, pos+ext);
+  return Rect3(pos, pos + ext);
 }
 
 Rect3 LocalDomain::get_compute_region() const noexcept {
@@ -51,4 +51,119 @@ void LocalDomain::swap() noexcept {
   CUDA_RUNTIME(cudaMemcpy(devCurrDataPtrs_, currDataPtrs_.data(), currDataPtrs_.size() * sizeof(currDataPtrs_[0]),
                           cudaMemcpyHostToDevice));
   nvtxRangePop();
+}
+
+Dim3 LocalDomain::halo_pos(const Dim3 &dir, const bool halo) const noexcept {
+  assert(dir.all_gt(-2));
+  assert(dir.all_lt(2));
+
+  Dim3 ret;
+
+  // +xhalo is the left edge + -x radius + the interior
+  // +x interior is just the left edge + interior size
+  if (1 == dir.x) {
+    ret.x = sz_.x + (halo ? radius_.x(-1) : 0);
+  } else if (-1 == dir.x) {
+    ret.x = halo ? 0 : radius_.x(-1);
+  } else if (0 == dir.x) {
+    ret.x = radius_.x(-1);
+  } else {
+    LOG_FATAL("unreachable");
+  }
+
+  if (1 == dir.y) {
+    ret.y = sz_.y + (halo ? radius_.y(-1) : 0);
+  } else if (-1 == dir.y) {
+    ret.y = halo ? 0 : radius_.y(-1);
+  } else if (0 == dir.y) {
+    ret.y = radius_.y(-1);
+  } else {
+    LOG_FATAL("unreachable");
+  }
+
+  if (1 == dir.z) {
+    ret.z = sz_.z + (halo ? radius_.z(-1) : 0);
+  } else if (-1 == dir.z) {
+    ret.z = halo ? 0 : radius_.z(-1);
+  } else if (0 == dir.z) {
+    ret.z = radius_.z(-1);
+  } else {
+    LOG_FATAL("unreachable");
+  }
+
+  return ret;
+}
+
+std::vector<unsigned char> LocalDomain::region_to_host(const Dim3 &pos, const Dim3 &ext,
+                                          const size_t qi // quantity index
+                                          ) const {
+
+  const size_t bytes = elem_size(qi) * ext.flatten();
+
+  // pack quantity
+  CUDA_RUNTIME(cudaSetDevice(gpu()));
+  void *devBuf = nullptr;
+  CUDA_RUNTIME(cudaMalloc(&devBuf, bytes));
+  const dim3 dimBlock = Dim3::make_block_dim(ext, 512);
+  const dim3 dimGrid = (ext + Dim3(dimBlock) - 1) / (Dim3(dimBlock));
+  pack_kernel<<<dimGrid, dimBlock>>>(devBuf, curr_data(qi), raw_size(), pos, ext, elem_size(qi));
+  CUDA_RUNTIME(cudaDeviceSynchronize());
+
+  // copy quantity to host
+  std::vector<unsigned char> hostBuf(bytes);
+  CUDA_RUNTIME(cudaMemcpy(hostBuf.data(), devBuf, hostBuf.size(), cudaMemcpyDefault));
+
+  float *ptr = reinterpret_cast<float *>(hostBuf.data());
+
+  // free device buffer
+  CUDA_RUNTIME(cudaFree(devBuf));
+
+  return hostBuf;
+}
+
+void LocalDomain::realize() {
+  LOG_SPEW("in realize()");
+  CUDA_RUNTIME(cudaGetLastError());
+  assert(currDataPtrs_.size() == nextDataPtrs_.size());
+  assert(dataElemSize_.size() == nextDataPtrs_.size());
+
+  LOG_INFO("origin is " << origin_);
+
+  // allocate each data region
+  CUDA_RUNTIME(cudaSetDevice(dev_));
+  // int rank;
+  // MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+  // std::cerr << "r" << rank << " dev=" << dev_ << "\n";
+  for (int64_t i = 0; i < num_data(); ++i) {
+    assert(i < dataElemSize_.size());
+    int64_t elemSz = dataElemSize_[i];
+    LOG_SPEW("elemSz=" << elemSz);
+    LOG_SPEW("radius +x=" << radius_.x(1));
+    LOG_SPEW("radius -x=" << radius_.x(-1));
+    LOG_SPEW("radius +y=" << radius_.y(1));
+    LOG_SPEW("radius -y=" << radius_.y(-1));
+    LOG_SPEW("radius +z=" << radius_.z(1));
+    LOG_SPEW("radius -z=" << radius_.z(-1));
+
+    int64_t elemBytes = ((sz_.x + radius_.x(-1) + radius_.x(1)) * (sz_.y + radius_.y(-1) + radius_.y(1)) *
+                         (sz_.z + radius_.z(-1) + radius_.z(1))) *
+                        elemSz;
+    LOG_SPEW("allocate " << elemBytes << " bytes");
+    char *c = nullptr;
+    char *n = nullptr;
+    CUDA_RUNTIME(cudaMalloc(&c, elemBytes));
+    CUDA_RUNTIME(cudaMalloc(&n, elemBytes));
+    assert(uintptr_t(c) % elemSz == 0 && "allocation should be aligned");
+    assert(uintptr_t(n) % elemSz == 0 && "allocation should be aligned");
+    currDataPtrs_[i] = c;
+    nextDataPtrs_[i] = n;
+  }
+
+  CUDA_RUNTIME(cudaMalloc(&devCurrDataPtrs_, currDataPtrs_.size() * sizeof(currDataPtrs_[0])));
+  CUDA_RUNTIME(cudaMalloc(&devDataElemSize_, dataElemSize_.size() * sizeof(dataElemSize_[0])));
+  CUDA_RUNTIME(cudaMemcpy(devCurrDataPtrs_, currDataPtrs_.data(), currDataPtrs_.size() * sizeof(currDataPtrs_[0]),
+                          cudaMemcpyHostToDevice));
+  CUDA_RUNTIME(cudaMemcpy(devDataElemSize_, dataElemSize_.data(), dataElemSize_.size() * sizeof(dataElemSize_[0]),
+                          cudaMemcpyHostToDevice));
+  CUDA_RUNTIME(cudaGetLastError());
 }
