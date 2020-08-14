d333333cbf0199cfbf1b672d6506fed158d797ed
Carl Pearson <carl.w.pearson@gmail.com>
2020-08-14 11:03:12 -0600

    factor out some repeated numeric code

---
+++ src/numeric.cpp
@@ -0,0 +1,25 @@
+#include "stencil/numeric.hpp"
+
+#include <cmath>
+#include <algorithm>
+
+std::vector<int64_t> prime_factors(int64_t n) {
+  std::vector<int64_t> result;
+  if (0 == n) {
+    return result;
+  }
+  while (n % 2 == 0) {
+    result.push_back(2);
+    n = n / 2;
+  }
+  for (int i = 3; i <= sqrt(n); i = i + 2) {
+    while (n % i == 0) {
+      result.push_back(i);
+      n = n / i;
+    }
+  }
+  if (n > 2)
+    result.push_back(n);
+  std::sort(result.begin(), result.end(), [](int64_t a, int64_t b) { return b < a; });
+  return result;
+}
