ea5593a0955bae4cca91d973d3218fc3017727d2
Carl Pearson <carl.w.pearson@gmail.com>
2020-08-31 15:14:23 -0600

    IpcRecver::test_listen

---
+++ include/stencil/tx_ipc.hpp
@@ -0,0 +1,94 @@
+#pragma once
+
+#include <cuda_runtime.h>
+#include <mpi.h>
+
+/* \file
+Handle synchronization between MPI ranks on the same node.
+
+Also, provides a CUDA event that is shared between the IpcSender and IpcRecver, but does not directly use that event.
+The object that uses the IpcSender and Recver may use the shared event to help coordinate
+
+*/
+
+/*
+ */
+class IpcSender {
+
+  MPI_Request evtReq_; // async send of event to IpcRecvter
+  MPI_Request dstReq_; // async recv of destination CUDA device id
+  MPI_Request notReq_; // async send of notify() message
+
+  int srcRank_, dstRank_;
+  int srcDom_, dstDom_;
+  int srcDev_, dstDev_;
+  cudaEvent_t event_; // event to synchronize without MPI
+  cudaIpcEventHandle_t eventHandle_;
+
+  char junk_; // garbage to be sent as notifcation message
+
+public:
+  IpcSender();
+  IpcSender(int srcRank, int srcDom, // domain ID
+            int dstRank, int dstDom, // domain ID
+            int srcDev               // cuda ID
+  );
+  ~IpcSender();
+
+  // start async send of event and recv of destination device ID
+  void async_prepare();
+
+  // block until we have recved the destination device ID and sent the event
+  void wait_prepare();
+
+  // async send notify message
+  void async_notify();
+
+  // block until notify is complete
+  void wait_notify();
+
+  const cudaEvent_t &event() const noexcept { return event_; }
+  int dst_dev() const noexcept { return dstDev_; }
+};
+
+/*
+ */
+class IpcRecver {
+
+  MPI_Request evtReq_; // async recv of event from IpcSender
+  MPI_Request dstReq_; // async send of my CUDA device id
+  MPI_Request notReq_; // async recv of notify() message
+
+  int srcRank_, dstRank_;
+  int srcDom_, dstDom_; // domain ID
+  int srcDev_, dstDev_; // cuda ID
+  cudaEvent_t event_;   // to synchronize with sender
+  cudaIpcEventHandle_t eventHandle_;
+
+  char junk_; // garbage to be recved into for notification message
+
+public:
+  IpcRecver();
+  IpcRecver(int srcRank, int srcDom, // domain ID
+            int dstRank, int dstDom, // domain ID
+            int dstDev               // cuda ID
+  );
+  ~IpcRecver();
+
+  // start async send of destination device and recv of event
+  void async_prepare();
+
+  // block until we have recved the event and sent the destination device
+  void wait_prepare();
+
+  // start listening for notification
+  void async_listen();
+
+  // true if notification has been recieved
+  bool test_listen();
+
+  // block until got notification
+  void wait_listen();
+
+  const cudaEvent_t &event() const noexcept { return event_; }
+};
---
+++ src/tx_ipc.cpp
@@ -0,0 +1,110 @@
+#include "stencil/tx_ipc.hpp"
+
+#include "stencil/cuda_runtime.hpp"
+#include "stencil/logging.hpp"
+#include "stencil/tx_common.hpp"
+
+#include <cassert>
+
+int ipc_tag_payload(int a, int b) noexcept {
+  assert(a >= 0 && a <= 0xFF);
+  assert(b >= 0 && b <= 0xFF);
+  return ((a & 0xFF) << 8) | (b & 0xFF);
+}
+
+IpcSender::IpcSender() : event_(0) {}
+IpcSender::IpcSender(int srcRank, int srcDom, int dstRank, int dstDom, int srcDev)
+    : srcRank_(srcRank), dstRank_(dstRank), srcDom_(srcDom), dstDom_(dstDom), srcDev_(srcDev), dstDev_(-1), event_(0) {}
+
+IpcSender::~IpcSender() {
+  if (event_) {
+    CUDA_RUNTIME(cudaEventDestroy(event_));
+  }
+}
+
+void IpcSender::async_prepare() {
+
+  // create an event and associated handle
+  CUDA_RUNTIME(cudaSetDevice(srcDev_));
+  CUDA_RUNTIME(cudaEventCreate(&event_, cudaEventInterprocess | cudaEventDisableTiming));
+  CUDA_RUNTIME(cudaIpcGetEventHandle(&eventHandle_, event_));
+
+  // send the event handle
+  LOG_SPEW("Isend handle");
+  const int evtTag = make_tag<MsgKind::ColocatedEvt>(ipc_tag_payload(srcDom_, dstDom_));
+  MPI_Isend(&eventHandle_, sizeof(eventHandle_), MPI_BYTE, dstRank_, evtTag, MPI_COMM_WORLD, &evtReq_);
+
+  // Retrieve the destination device id
+  LOG_SPEW("Irecv destination device");
+  const int devTag = make_tag<MsgKind::ColocatedDev>(ipc_tag_payload(srcDom_, dstDom_));
+  MPI_Irecv(&dstDev_, 1, MPI_INT, dstRank_, devTag, MPI_COMM_WORLD, &dstReq_);
+
+  LOG_SPEW("done IpcSender::async_prepare");
+}
+
+void IpcSender::wait_prepare() {
+  // block until we have recved the device ID
+  LOG_SPEW("sender: wait for destination device");
+  MPI_Wait(&dstReq_, MPI_STATUS_IGNORE);
+
+  // block until we have sent event handle
+  LOG_SPEW("sender: wait to send event");
+  MPI_Wait(&evtReq_, MPI_STATUS_IGNORE);
+
+  LOG_SPEW("done IpcSender::wait_prepare");
+}
+
+void IpcSender::async_notify() {
+  const int notTag = make_tag<MsgKind::ColocatedNotify>(ipc_tag_payload(srcDom_, dstDom_));
+  MPI_Isend(&junk_, 1, MPI_BYTE, dstRank_, notTag, MPI_COMM_WORLD, &notReq_);
+}
+
+void IpcSender::wait_notify() { MPI_Wait(&notReq_, MPI_STATUS_IGNORE); }
+
+IpcRecver::IpcRecver() : event_(0) {}
+IpcRecver::IpcRecver(int srcRank, int srcDom, int dstRank, int dstDom, int dstDev)
+    : srcRank_(srcRank), dstRank_(dstRank), srcDom_(srcDom), dstDom_(dstDom), srcDev_(-1), dstDev_(dstDev), event_(0) {}
+
+IpcRecver::~IpcRecver() {
+  if (event_) {
+    CUDA_RUNTIME(cudaEventDestroy(event_));
+  }
+}
+
+void IpcRecver::async_prepare() {
+  const int payload = ((srcDom_ & 0xFF) << 8) | (dstDom_ & 0xFF);
+
+  // recv the event handle
+  const int evtTag = make_tag<MsgKind::ColocatedEvt>(payload);
+  MPI_Irecv(&eventHandle_, sizeof(eventHandle_), MPI_BYTE, srcRank_, evtTag, MPI_COMM_WORLD, &evtReq_);
+
+  // Send the CUDA device id to the ColocatedSender
+  const int dstTag = make_tag<MsgKind::ColocatedDev>(payload);
+  MPI_Isend(&dstDev_, 1, MPI_INT, srcRank_, dstTag, MPI_COMM_WORLD, &dstReq_);
+}
+
+void IpcRecver::wait_prepare() {
+  // wait to recv the event
+  MPI_Wait(&evtReq_, MPI_STATUS_IGNORE);
+
+  // convert event handle to event
+  CUDA_RUNTIME(cudaSetDevice(dstDev_));
+  CUDA_RUNTIME(cudaIpcOpenEventHandle(&event_, eventHandle_));
+
+  // wait to send the mem handle and the CUDA device ID
+  MPI_Wait(&dstReq_, MPI_STATUS_IGNORE);
+}
+
+void IpcRecver::async_listen() {
+  // wait to recv the event
+  const int notTag = make_tag<MsgKind::ColocatedNotify>(ipc_tag_payload(srcDom_, dstDom_));
+  MPI_Irecv(&junk_, 1, MPI_INT, srcRank_, notTag, MPI_COMM_WORLD, &notReq_);
+}
+
+bool IpcRecver::test_listen() {
+  int flag;
+  MPI_Test(&notReq_, &flag, MPI_STATUS_IGNORE);
+  return flag;
+}
+
+void IpcRecver::wait_listen() { MPI_Wait(&notReq_, MPI_STATUS_IGNORE); }
