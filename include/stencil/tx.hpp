cabe025cf1126e1857d9a3c2221886ad6d62b4b0
Carl Pearson <carl.w.pearson@gmail.com>
2020-09-24 09:02:30 -0600

    fix issue if CUDA is not used

--- include/stencil/tx.hpp
+++ include/stencil/tx.hpp
@@ -2,6 +2,9 @@
 
 #include "tx_common.hpp"
 
-#if STENCIL_USE_CUDA == 1 && __NVCC__
+#if STENCIL_USE_CUDA == 1 && defined(__NVCC__)
 #include "tx_cuda.cuh"
+#else
+class PeerCopySender {}; // no-op without CUDA
+class PeerAccessSender {}; // no-op without CUDA
 #endif
