5ee100caf5a2850769e6c75295c12c57ded37bae
Carl Pearson <carl.w.pearson@gmail.com>
2020-09-24 16:44:28 -0600

    all compilation units agree on object size

--- include/stencil/tx.hpp
+++ include/stencil/tx.hpp
@@ -4,7 +4,4 @@
 
 #if STENCIL_USE_CUDA == 1 && defined(__NVCC__)
 #include "tx_cuda.cuh"
-#else
-class PeerCopySender {}; // no-op without CUDA
-class PeerAccessSender {}; // no-op without CUDA
 #endif
