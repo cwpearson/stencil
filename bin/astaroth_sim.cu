1638b3c0e075a0c3fa816a4f43f46b94da577e86
Carl Pearson <carl.w.pearson@gmail.com>
2020-12-21 15:02:44 -0700

    remove cxxopts, start work on astaroth

--- bin/astaroth_sim.cu
+++ bin/astaroth_sim.cu
@@ -1,12 +1,29 @@
+/* Try to do some rough approximation of astaroth using the stencil library.
+
+
+git clone https://jpekkila@bitbucket.org/jpekkila/astaroth.git
+cd astaroth && mkdir build && cd build
+cmake -DDOUBLE_PRECISION=ON -DMPI_ENABLED=ON .. && make -j
+
+First, we will try to understand the required data structures to create them.
+
+acHostMeshCreate() in src/astaroth.cc seems to suggest 8 handles that are nx/ny/nz * sizeof(real)
+similar in acDeviceCreate in src/device.cc
+
+We will assume real as double as the case we care about
+
+*/
+
 #include <chrono>
 #include <cmath>
 #include <thread>
 
 #include <nvToolsExt.h>
 
-#include <cxxopts/cxxopts.hpp>
-
+#include "argparse/argparse.hpp"
 #include "stencil/stencil.hpp"
+
+#define Real double
 
 /*! set compute region to dst[x,y,z] = sin(x+y+z + origin.x + origin.y + origin.z)
  */
@@ -26,7 +43,7 @@ __global__ void init_kernel(Accessor<T> dst,    //<! [out] pointer to beginning 
 
 /* Apply the stencil to the coordinates in `reg`
  */
-__global__ void stencil_kernel(Accessor<float> dst, const Accessor<float> src, const Rect3 reg) {
+__global__ void stencil_kernel(Accessor<Real> dst, const Accessor<Real> src, const Rect3 reg) {
 
   for (int64_t z = reg.lo.z + blockIdx.z * blockDim.z + threadIdx.z; z < reg.hi.z; z += gridDim.z * blockDim.z) {
     for (int64_t y = reg.lo.y + blockIdx.y * blockDim.y + threadIdx.y; y < reg.hi.y; y += gridDim.y * blockDim.y) {
@@ -48,34 +65,38 @@ __global__ void stencil_kernel(Accessor<float> dst, const Accessor<float> src, c
 
 int main(int argc, char **argv) {
 
-  cxxopts::Options options("MyProgram", "One line description of MyProgram");
-  // clang-format off
-  options.add_options()
-  ("h,help", "Show help")
-  ("remote", "Enable RemoteSender/Recver")
-  ("colocated", "Enable ColocatedHaloSender/Recver")
-  ("peer", "Enable PeerAccessSender")
-  ("kernel", "Enable PeerCopySender")
-  ("trivial", "Skip node-aware placement")
-  ("x", "x dim", cxxopts::value<int>()->default_value("512"))
-  ("y", "y dim", cxxopts::value<int>()->default_value("512"))
-  ("z", "z dim", cxxopts::value<int>()->default_value("512"))
-  ("f,file", "File name", cxxopts::value<std::string>());
-  // clang-format on
-
-  auto result = options.parse(argc, argv);
-
-  if (result["help"].as<bool>()) {
-    std::cerr << options.help();
-    exit(EXIT_SUCCESS);
-  }
-
   MPI_Init(&argc, &argv);
-
   int size;
   int rank;
   MPI_Comm_size(MPI_COMM_WORLD, &size);
   MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+
+  argparse::Parser p("an astaroth-like performance simulator");
+
+  int x = 256;
+  int y = 256;
+  int z = 256;
+  bool trivialPlacement = false;
+
+  p.add_flag(trivialPlacement, "--trivial")->help("use trivial placement");
+  p.add_positional(x)->required();
+  p.add_positional(y)->required();
+  p.add_positional(z)->required();
+
+  // If there was an error during parsing, report it.
+  if (!p.parse(argc, argv)) {
+    if (0 == rank) {
+      std::cerr << p.help();
+    }
+    exit(EXIT_FAILURE);
+  }
+
+  if (p.need_help()) {
+    if (0 == rank) {
+      std::cerr << p.help();
+    }
+    exit(EXIT_SUCCESS);
+  }
 
   int devCount;
   CUDA_RUNTIME(cudaGetDeviceCount(&devCount));
@@ -90,49 +111,17 @@ int main(int argc, char **argv) {
     std::cout << "assuming " << numSubdoms << " subdomains\n";
   }
 
-  double kernelMillis = 50;
-  size_t x = result["x"].as<int>();
-  size_t y = result["y"].as<int>();
-  size_t z = result["z"].as<int>();
-
-  cudaDeviceProp prop;
-  CUDA_RUNTIME(cudaGetDeviceProperties(&prop, 0));
-  if (std::string("Tesla V100-SXM2-32GB") == prop.name) {
-    kernelMillis = 20.1;
-  } else if (std::string("Tesla P100-SXM2-16GB") == prop.name) {
-    kernelMillis = 34.1;
-  } else {
-    if (0 == rank) {
-      std::cerr << "WARN: unknown GPU " << prop.name << ", using " << kernelMillis << "ms for kernel\n";
-    }
-  }
-
-  /*
-  Table 5
-  512^3
-  512^3 on Pascal 34.1ms
-  512^3 on Volta  20.1ms
-  */
-
   Method methods = Method::None;
-  if (result["remote"].as<bool>()) {
-    methods |= Method::CudaMpi;
-  }
-  if (result["colocated"].as<bool>()) {
-    methods |= Method::ColoPackMemcpyUnpack;
-  }
-  if (result["peer"].as<bool>()) {
-    methods |= Method::CudaMemcpyPeer;
-  }
-  if (result["kernel"].as<bool>()) {
-    methods |= Method::CudaKernel;
-  }
+  methods |= Method::CudaMpi;
+  methods |= Method::ColoPackMemcpyUnpack;
+  methods |= Method::CudaMemcpyPeer;
+  methods |= Method::CudaKernel;
   if (Method::None == methods) {
     methods = Method::Default;
   }
 
   PlacementStrategy strategy = PlacementStrategy::NodeAware;
-  if (result["trivial"].as<bool>()) {
+  if (trivialPlacement) {
     strategy = PlacementStrategy::Trivial;
   }
 
@@ -149,16 +138,20 @@ int main(int argc, char **argv) {
     dd.set_radius(radius);
     dd.set_placement(strategy);
 
-    auto dh0 = dd.add_data<float>("d0");
-    // auto dh1 = dd.add_data<float>("d1");
-    // auto dh2 = dd.add_data<float>("d2");
-    // auto dh3 = dd.add_data<float>("d3");
+    auto dh0 = dd.add_data<Real>("d0");
+    auto dh1 = dd.add_data<Real>("d1");
+    auto dh2 = dd.add_data<Real>("d2");
+    auto dh3 = dd.add_data<Real>("d3");
+    auto dh4 = dd.add_data<Real>("d4");
+    auto dh5 = dd.add_data<Real>("d5");
+    auto dh6 = dd.add_data<Real>("d6");
+    auto dh7 = dd.add_data<Real>("d7");
 
     dd.realize();
 
     MPI_Barrier(MPI_COMM_WORLD);
 
-    // create a compute stream for each local domain
+    // create a stream for the integration kernels to run in
     std::vector<RcStream> computeStreams(dd.domains().size());
     for (size_t di = 0; di < dd.domains().size(); ++di) {
       computeStreams[di] = RcStream(dd.domains()[di].gpu());
@@ -185,8 +178,8 @@ int main(int argc, char **argv) {
       // launch operations on interior
       for (size_t di = 0; di < dd.domains().size(); ++di) {
         auto &d = dd.domains()[di];
-        const Accessor<float> src0 = d.get_curr_accessor<float>(dh0);
-        const Accessor<float> dst0 = d.get_next_accessor<float>(dh0);
+        const Accessor<Real> src0 = d.get_curr_accessor<Real>(dh0);
+        const Accessor<Real> dst0 = d.get_next_accessor<Real>(dh0);
         nvtxRangePush("launch");
         const Rect3 cr = interiors[di];
         std::cerr << rank << ": launch on region=" << cr << " (interior)\n";
@@ -207,8 +200,8 @@ int main(int argc, char **argv) {
       // operate on exterior
       for (size_t di = 0; di < dd.domains().size(); ++di) {
         auto &d = dd.domains()[di];
-        const Accessor<float> src0 = d.get_curr_accessor<float>(dh0);
-        const Accessor<float> dst0 = d.get_next_accessor<float>(dh0);
+        const Accessor<Real> src0 = d.get_curr_accessor<Real>(dh0);
+        const Accessor<Real> dst0 = d.get_next_accessor<Real>(dh0);
         for (size_t si = 0; si < exteriors[di].size(); ++si) {
           nvtxRangePush("launch");
           const Rect3 cr = exteriors[di][si];
