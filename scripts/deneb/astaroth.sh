456146ed0d6c9d17f04c640fd22694082071c4c8
Carl Pearson <carl.w.pearson@gmail.com>
2021-02-07 16:02:03 -0700

    add --trivial flag

--- scripts/deneb/astaroth.sh
+++ scripts/deneb/astaroth.sh
@@ -11,13 +11,11 @@
 
 set -eou pipefail
 
-module reset
-module load gcc
-module load cuda/11.0.3
-module load nsight-systems/2020.3.1.71
-
-DIR=/gpfs/alpine/csc362/scratch/cpearson/stencil_results
+DIR=$HOME/sync_work/stencil_results
 OUT=$DIR/astaroth.csv
+
+MPIRUN=$HOME/software/openmpi-4.0.5/bin/mpirun
+# MPIRUN=mpirun
 
 set -x
 
@@ -26,13 +24,13 @@ echo "" > $OUT
 
 for flags in "--staged" "--staged --colo" "--staged --colo --peer" "--staged --colo --peer --kernel" "--trivial --staged --colo --peer --kernel"; do
   echo $flags >> $OUT
-  echo "nodes,ranks/node,ranks,x,y,z,iter (s),exch (s)" >> $OUT
-  for nodes in 1 2 4 8 16 32 64; do
+  echo "nodes,ranks/node,ranks,x,y,z,iter (s),exch (s)" | tee -a $OUT
+  for nodes in 1; do
     for rpn in 1 2 6; do
       let n=$nodes*$rpn
       echo -n "${nodes},${rpn}," | tee -a $OUT
-      jsrun --smpiargs="-gpu" -n $n -r $rpn -a 1 -g 1 -c 7 -b rs ../../build/astaroth/astaroth $flags | tee -a $OUT
-      #jsrun --smpiargs="-gpu" -n $n -r $rpn -a 1 -g 1 -c 7 -b rs nsys profile -t cuda,nvtx -f true -o $DIR/astaroth_${nodes}n_${rpn}rpn_${X}x_%q{OMPI_COMM_WORLD_RANK} ../../build/astaroth/astaroth
+      $MPIRUN -n $n ../../build/astaroth/astaroth $flags | tee -a $OUT
+      #$MPIRUN -n $n nsys profile -t cuda,nvtx -f true -o $DIR/astaroth_${nodes}n_${rpn}rpn_%q{OMPI_COMM_WORLD_RANK} ../../  build/astaroth/astaroth $flags
     done
   done
   echo "" >> $OUT
