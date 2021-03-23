87f53e9a6a580791f30a8482253838477bae820b
Carl Pearson <carl.w.pearson@gmail.com>
2021-03-23 17:26:25 -0600

    explosion kernel

--- astaroth/astaroth.cu
+++ astaroth/astaroth.cu
@@ -14,9 +14,19 @@ Try to do some rough approximation of astaroth using the stencil library.
 #include "kernels.h"
 #include "statistics.hpp"
 
+// handle IDs
+const int H_LNRHO = 0;
+const int H_UUX = 1;
+const int H_UUY = 2;
+const int H_UUZ = 3;
+const int H_AX = 4;
+const int H_AY = 5;
+const int H_AZ = 6;
+const int H_ENTROPY = 7;
+
 template <typename T>
-static __global__ void periodic_ramp_init_kernel(Accessor<T> dst, //<! [out] region to fill
-                                                 Rect3 dstExt     //<! [in] the extent of the region to initialize
+static __global__ void sin_ramp_init_kernel(Accessor<T> dst, //<! [out] region to fill
+                                            Rect3 dstExt     //<! [in] the extent of the region to initialize
 ) {
   const T ripple[4] = {0, 0.25, 0, -0.25};
   const size_t period = sizeof(ripple) / sizeof(ripple[0]);
@@ -37,6 +47,31 @@ static __global__ void periodic_ramp_init_kernel(Accessor<T> dst, //<! [out] reg
   }
 }
 
+template <typename T>
+static __global__ void sin_init_kernel(Accessor<T> dst,    //<! [out] region to fill
+                                       const Rect3 dstExt, //<! [in] the extent of the region to initialize
+                                       const Dim3 totExt   //<! [in] the total size of the distributed region
+) {
+  const T amplUU = 0.0001;
+  const T period = 16;
+  const T pi = 3.141592653589793238462643383;
+
+  const size_t tiz = blockDim.z * blockIdx.z + threadIdx.z;
+  const size_t tiy = blockDim.y * blockIdx.y + threadIdx.y;
+  const size_t tix = blockDim.x * blockIdx.x + threadIdx.x;
+
+  for (size_t z = dstExt.lo.z + tiz; z < dstExt.hi.z; z += gridDim.z * blockDim.z) {
+    for (size_t y = dstExt.lo.y + tiy; y < dstExt.hi.y; y += gridDim.y * blockDim.y) {
+      for (size_t x = dstExt.lo.x + tix; x < dstExt.hi.x; x += gridDim.x * blockDim.x) {
+
+        T val = amplUU * sin(float(y) * 2 * pi / period);
+        Dim3 p(x, y, z);
+        dst[p] = val;
+      }
+    }
+  }
+}
+
 static __device__ uint32_t hash32(uint32_t x) {
   x = ((x >> 16) ^ x) * 0x45d9f3b;
   x = ((x >> 16) ^ x) * 0x45d9f3b;
@@ -51,9 +86,10 @@ static __device__ uint64_t hash64(uint64_t x) {
   return x;
 }
 
+/* bad random numbers from -1 to 1 */
 template <typename T>
 static __global__ void hash_init_kernel(Accessor<T> dst, //<! [out] region to fill
-                                       Rect3 dstExt     //<! [in] the extent of the region to initialize
+                                        Rect3 dstExt     //<! [in] the extent of the region to initialize
 ) {
 
   const size_t tiz = blockDim.z * blockIdx.z + threadIdx.z;
@@ -65,8 +101,143 @@ static __global__ void hash_init_kernel(Accessor<T> dst, //<! [out] region to fi
       for (size_t x = dstExt.lo.x + tix; x < dstExt.hi.x; x += gridDim.x * blockDim.x) {
 
         Dim3 p(x, y, z);
+        // create a "random" number from 0 to 1
         T val = float(hash64(p.x) ^ hash64(p.y) ^ hash64(p.z)) / float(uint64_t(-1));
+        // shift it to be -1 to 1
+        val = (val - 0.5) * 2;
         dst[p] = val;
+      }
+    }
+  }
+}
+
+template <typename T>
+static __global__ void const_init_kernel(Accessor<T> dst, //<! [out] region to fill
+                                         Rect3 dstExt,    //<! [in] the extent of the region to initialize
+                                         T val) {
+
+  const size_t tiz = blockDim.z * blockIdx.z + threadIdx.z;
+  const size_t tiy = blockDim.y * blockIdx.y + threadIdx.y;
+  const size_t tix = blockDim.x * blockIdx.x + threadIdx.x;
+
+  for (size_t z = dstExt.lo.z + tiz; z < dstExt.hi.z; z += gridDim.z * blockDim.z) {
+    for (size_t y = dstExt.lo.y + tiy; y < dstExt.hi.y; y += gridDim.y * blockDim.y) {
+      for (size_t x = dstExt.lo.x + tix; x < dstExt.hi.x; x += gridDim.x * blockDim.x) {
+        Dim3 p(x, y, z);
+        dst[p] = val;
+      }
+    }
+  }
+}
+
+template <typename T>
+static __global__ void radial_explosion_init_kernel(Accessor<T> uux, Accessor<T> uuy, Accessor<T> uuz,
+                                                    const Rect3 dstExt, //<! [in] the extent of the region to initialize
+                                                    const Dim3 totExt //<! [in] the total size of the distributed region
+) {
+  const T amplUU = 1;
+  const T shellRadiusUU = 0.8;
+  const T widthUU = 0.2;
+
+  // taken from conf
+  const T dsx = 0.04908738521;
+  const T dsy = 0.04908738521;
+  const T dsz = 0.04908738521;
+
+  const Dim3 orig(0.01, 32 * dsy, 64 * dsz);
+
+  const size_t tiz = blockDim.z * blockIdx.z + threadIdx.z;
+  const size_t tiy = blockDim.y * blockIdx.y + threadIdx.y;
+  const size_t tix = blockDim.x * blockIdx.x + threadIdx.x;
+
+  for (size_t z = dstExt.lo.z + tiz; z < dstExt.hi.z; z += gridDim.z * blockDim.z) {
+    for (size_t y = dstExt.lo.y + tiy; y < dstExt.hi.y; y += gridDim.y * blockDim.y) {
+      for (size_t x = dstExt.lo.x + tix; x < dstExt.hi.x; x += gridDim.x * blockDim.x) {
+
+        const Dim3 p(x, y, z);
+        T xx = abs(x * dsx - orig.x);
+        T yy = abs(y * dsy - orig.y);
+        T zz = abs(z * dsz - orig.z);
+        // printf("%f %f %f\n", xx, yy, zz);
+        const T rr2 = pow(xx, 2.0) + pow(yy, 2.0) + pow(zz, 2.0);
+        const T rr = sqrt(rr2);
+
+        T theta, phi, uu_radial;
+
+        if (rr > 0) {
+          if (zz >= 0) {
+            // got a hair over 1.0 here for some reason
+            theta = acos(min(1.0, zz / rr));
+          } else {
+            zz = -zz;
+            theta = M_PI - acos(zz / rr);
+          }
+
+          if (xx != 0.0) {
+            if (xx < 0.0 && yy >= 0.0) {
+              //-+
+              xx = -xx; // Needs a posite value for atan
+              phi = M_PI - atan(yy / xx);
+
+            } else if (xx > 0.0 && yy < 0.0) {
+              //+-
+              yy = -yy;
+              phi = 2.0 * M_PI - atan(yy / xx);
+            } else if (xx < 0.0 && yy < 0.0) {
+              //--
+              yy = -yy;
+              xx = -xx;
+              phi = M_PI + atan(yy / xx);
+            } else {
+              //++
+              phi = atan(yy / xx);
+            }
+          } else { // To avoid div by zero with atan
+            if (yy > 0.0) {
+              phi = M_PI / 2.0;
+            } else if (yy < 0.0) {
+              phi = (3.0 * M_PI) / 2.0;
+            } else {
+              phi = 0.0;
+            }
+          }
+
+          // Set zero for explicit safekeeping
+          if (xx == 0.0 && yy == 0.0) {
+            phi = 0.0;
+          }
+
+          // Gaussian velocity
+          // uu_radial = AMPL_UU*exp( -rr2 / (2.0*pow(WIDTH_UU, 2.0)) );
+          // New distribution, where that gaussion wave is not in the exact centre coordinates
+          // uu_radial = AMPL_UU*exp( -pow((rr - 4.0*WIDTH_UU),2.0) / (2.0*pow(WIDTH_UU, 2.0)) ); //TODO: Parametrize
+          // the peak location.
+          uu_radial = amplUU * exp(-pow((rr - shellRadiusUU), 2.0) / (2.0 * pow(widthUU, 2.0)));
+
+          if (zz < 1.0 && false)
+            printf("%f %f %f -> %f / %f -> %f -> %f\n", xx, yy, zz, -pow((rr - shellRadiusUU), 2.0),
+                   (2.0 * pow(widthUU, 2.0)), exp(-pow((rr - shellRadiusUU), 2.0) / (2.0 * pow(widthUU, 2.0))),
+                   uu_radial);
+
+        } else {
+          uu_radial = 0.0; // TODO: There will be a discontinuity in the origin... Should the shape of the distribution
+                           // be different?
+        }
+
+        // printf("%f %f %f\n", uu_radial, theta, phi);
+
+        // Determine the cartesian velocity components and lnrho
+        uux[p] = uu_radial * sin(theta) * cos(phi);
+        uuy[p] = uu_radial * sin(theta) * sin(phi);
+        uuz[p] = uu_radial * cos(theta);
+
+        // if (isnan(uux[p])) {
+        //   printf("ERR: %e %f %f %f\n", zz/rr, acos(zz/rr), theta, sin(theta));
+        // }
+
+        // if (uu_radial > 0) {
+        //   printf("[%lu %lu %lu] %.9f %.9f %.9f\n", p.x, p.y, p.z, uu_radial, theta, phi);
+        // }
       }
     }
   }
@@ -190,21 +361,22 @@ int main(int argc, char **argv) {
   acLoadConfig(AC_DEFAULT_CONFIG, &info);
 
   // figure out the whole domain size
+  Dim3 totExt(info.int_params[AC_nx], info.int_params[AC_ny], info.int_params[AC_nz]);
   {
 #if 0
     int3 i3 = decompose_xyz(ranksPerNode);
-    info.int_params[AC_nx] *= i3.x;
-    info.int_params[AC_ny] *= i3.y;
-    info.int_params[AC_nz] *= i3.z;
+    totExt.x *= i3.x;
+    totExt.y *= i3.y;
+    totExt.z *= i3.z;
     i3 = decompose_xyz(numNodes);
-    info.int_params[AC_nx] *= i3.x;
-    info.int_params[AC_ny] *= i3.y;
-    info.int_params[AC_nz] *= i3.z;
+    totExt.x *= i3.x;
+    totExt.y *= i3.y;
+    totExt.z *= i3.z;
 #else
     int3 i3 = decompose_zyx(size);
-    info.int_params[AC_nx] *= i3.x;
-    info.int_params[AC_ny] *= i3.y;
-    info.int_params[AC_nz] *= i3.z;
+    totExt.x *= i3.x;
+    totExt.y *= i3.y;
+    totExt.z *= i3.z;
 #endif
   }
 
@@ -222,9 +394,7 @@ int main(int argc, char **argv) {
     std::cerr << "AC_ny_max=" << info.int_params[AC_ny_max] << "\n";
     std::cerr << "AC_nz_max=" << info.int_params[AC_nz_max] << "\n";
   }
-  const int x = info.int_params[AC_nx];
-  const int y = info.int_params[AC_ny];
-  const int z = info.int_params[AC_nz];
+
   MPI_Barrier(MPI_COMM_WORLD);
 
   PlacementStrategy strategy = PlacementStrategy::NodeAware;
@@ -239,7 +409,7 @@ int main(int argc, char **argv) {
   { // scope domains before mpi_finalize
     size_t radius = 3;
 
-    DistributedDomain dd(x, y, z);
+    DistributedDomain dd(totExt.x, totExt.y, totExt.z);
 
     dd.set_methods(methods);
     dd.set_radius(radius);
@@ -247,8 +417,18 @@ int main(int argc, char **argv) {
 
     // add required data
     std::vector<DataHandle<AcReal>> handles;
-    for (size_t i = 0; i < NUM_VTXBUF_HANDLES; ++i) {
-      handles.push_back(dd.add_data<AcReal>(""));
+    // create NUM_VTXBUF_HANDLES values
+    handles.push_back(dd.add_data<AcReal>("lnrho"));
+    handles.push_back(dd.add_data<AcReal>("uux"));
+    handles.push_back(dd.add_data<AcReal>("uuy"));
+    handles.push_back(dd.add_data<AcReal>("uuz"));
+    handles.push_back(dd.add_data<AcReal>("ax"));
+    handles.push_back(dd.add_data<AcReal>("ay"));
+    handles.push_back(dd.add_data<AcReal>("az"));
+    handles.push_back(dd.add_data<AcReal>("entropy"));
+    if (handles.size() != NUM_VTXBUF_HANDLES) {
+      std::cerr << "ERROR\n";
+      exit(EXIT_FAILURE);
     }
 
     // create arrays
@@ -272,8 +452,6 @@ int main(int argc, char **argv) {
     for (size_t di = 0; di < dd.domains().size(); ++di) {
       int device = dd.domains()[di].gpu();
       acDeviceLoadDefaultUniforms(device);
-
-      std::cerr << info.int_params[AC_nx] << "\n";
       acDeviceLoadMeshInfo(device, info);
     }
 
@@ -283,9 +461,11 @@ int main(int argc, char **argv) {
       VertexBufferArray &vba = vbas[di];
       LocalDomain &d = dd.domains()[di];
 
+      // set the appropriate in and out buffers
       for (size_t i = 0; i < NUM_VTXBUF_HANDLES; ++i) {
         vba.in[i] = d.get_curr_accessor<AcReal>(handles[i]).ptr().ptr;
         vba.out[i] = d.get_next_accessor<AcReal>(handles[i]).ptr().ptr;
+        std::cerr << rank << ": out[" << i << "]=" << vba.out[i] << "\n";
       }
     }
 
@@ -296,10 +476,39 @@ int main(int argc, char **argv) {
       d.set_device();
       dim3 dimBlock = Dim3::make_block_dim(d.raw_size(), 512);
       dim3 dimGrid = ((d.raw_size()) + Dim3(dimBlock) - 1) / (Dim3(dimBlock));
+
+      // initialize all
+#if 0
       for (DataHandle<AcReal> dh : handles) {
         hash_init_kernel<<<dimGrid, dimBlock, 0, cStreamInterior[di]>>>(d.get_curr_accessor(dh),
-                                                                                 d.get_compute_region());
+                                                                        d.get_compute_region());
       }
+#endif
+
+      // sine wave lnrho
+#if 0
+      sin_init_kernel<<<dimGrid, dimBlock, 0, cStreamInterior[di]>>>(d.get_curr_accessor(handles[H_LNRHO]),
+                                                                     d.get_compute_region(), totExt);
+#endif
+
+      // radial explostion
+#if 1
+      // everything to random numbers
+      for (DataHandle<AcReal> dh : handles) {
+        hash_init_kernel<<<dimGrid, dimBlock, 0, cStreamInterior[di]>>>(d.get_curr_accessor(dh),
+                                                                        d.get_compute_region());
+      }
+
+      // constant for lnrho
+      const_init_kernel<<<dimGrid, dimBlock, 0, cStreamInterior[di]>>>(d.get_curr_accessor(handles[H_LNRHO]),
+                                                                       d.get_compute_region(), 1.0);
+
+      // radial explosion for velocity
+      radial_explosion_init_kernel<<<dimGrid, dimBlock, 0, cStreamInterior[di]>>>(
+          d.get_curr_accessor(handles[H_UUX]), d.get_curr_accessor(handles[H_UUY]), d.get_curr_accessor(handles[H_UUZ]),
+          d.get_compute_region(), totExt);
+#endif
+
       CUDA_RUNTIME(cudaDeviceSynchronize());
       std::cerr << "init done\n";
     }
@@ -320,7 +529,7 @@ int main(int argc, char **argv) {
     // we will need to add in the offset from the stencil region
     const Dim3 acOff = Dim3(STENCIL_ORDER / 2, STENCIL_ORDER / 2, STENCIL_ORDER / 2);
 
-    for (size_t iter = 0; iter < 1; ++iter) {
+    for (size_t iter = 0; iter < 10; ++iter) {
       double exchElapsed = 0;
       MPI_Barrier(MPI_COMM_WORLD);
       double iterStart = MPI_Wtime();
@@ -345,14 +554,15 @@ int main(int argc, char **argv) {
             // std::cerr << rank << ": launch on region=" << cr << " (interior)\n";
             // std::cerr << src0.origin() << "=src0 origin\n";
             d.set_device();
-            acDeviceLoadScalarUniform(d.gpu(), cStreamInterior[di], AC_dt, AC_REAL_EPSILON);
+            // acDeviceLoadScalarUniform(d.gpu(), cStreamInterior[di], AC_dt, AC_REAL_EPSILON);
+            acDeviceLoadScalarUniform(d.gpu(), cStreamInterior[di], AC_dt, 1e-7);
             integrate_substep(substep, cStreamInterior[di], cr, vbas[di]);
             nvtxRangePop(); // launch
           }
         }
 
         // exchange halo
-        std::cerr << rank << ": exchange\n";
+        std::cerr << rank << ": exchange " << iter << "\n";
         double exchStart = MPI_Wtime();
         dd.exchange();
         exchElapsed += MPI_Wtime() - exchStart;
@@ -390,6 +600,11 @@ int main(int argc, char **argv) {
       }
       // swap inputs and outputs
       dd.swap();
+      // swap vbas
+      for (size_t di = 0; di < dd.domains().size(); ++di) {
+        VertexBufferArray &vba = vbas[di];
+        std::swap(vba.in, vba.out);
+      }
 
       double iterElapsed = MPI_Wtime() - iterStart;
 
@@ -397,6 +612,13 @@ int main(int argc, char **argv) {
       MPI_Allreduce(MPI_IN_PLACE, &exchElapsed, 1, MPI_DOUBLE, MPI_MAX, MPI_COMM_WORLD);
       iterTime.insert(iterElapsed);
       exchTime.insert(exchElapsed);
+
+      if (iter % 100 == 99) {
+        std::stringstream ss;
+        ss << "iter" << iter;
+        dd.write_paraview(ss.str());
+        MPI_Barrier(MPI_COMM_WORLD);
+      }
     }
 
     if (paraviewFinal) {
